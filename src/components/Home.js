539663cfbc7fec1e29e9cca474f5ab638ec8a424
bat098 <mateusz.baciak@gmail.com>
2021-06-10 11:28:02 +0200

    add redux for login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchLogin } from '../redux/auth/authActions'
 
 const Home = () => {
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.auth.data)
+    console.log(user)
+
     return (
         <div>
-            home
+            <button onClick={() => dispatch(fetchLogin())}>Click</button>
         </div>
     )
 }
---
+++ src/redux/auth/authReducer.js
@@ -0,0 +1,31 @@
+import { FETCH_LOGIN_REQUEST, FETCH_LOGIN_SUCCESS, FETCH_LOGIN_FAILURE } from './authTypes'
+
+const initialState = {
+    loading: false,
+    data: [],
+    error: ''
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_LOGIN_REQUEST: return {
+            ...state,
+            loading: true
+        }
+        case FETCH_LOGIN_SUCCESS: return {
+            ...state,
+            loading: false,
+            data: action.payload,
+            error: ''
+        }
+        case FETCH_LOGIN_FAILURE: return {
+            ...state,
+            loading: false,
+            data: [],
+            error: action.payload
+        }
+        default: return state
+    }
+}
+
+export default authReducer
